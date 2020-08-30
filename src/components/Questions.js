123970277e70ed1c29f30f0a06570505f71bf5cf
tgrimault21 <t.grimault21@gmail.com>
2020-08-30 13:51:38 +0200

    Task 'Ask Did XX Star in ? questions' done

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import useSWR from 'swr';
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -9,16 +8,12 @@ function shuffle(array) {
   return array
 }
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Questions(props) {
 
   const [questionCount, setQuestionCount] = useState(0)
   const [score, setScore] = useState(0)
-  const [fetchActor, setFetchActor] = useState(undefined)
   
-  const { data, error } = useSWR(() => fetchActor ? 'https://api.themoviedb.org/3/person/' + fetchActor + '/movie_credits?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US' : null, fetcher)
-
   const questions = []
   
   shuffle(props.actors)
@@ -41,23 +36,33 @@ export default function Questions(props) {
   })
 
   function answer(yes, actor, movie) {
-    setFetchActor(actor.id)
-
-    const answerIs = actor.movies.find(actorMovie => actorMovie === movie)
-
-    if ((yes && answerIs) || (!yes && !answerIs)) {
-      setScore(score+1)
-    }
-
-    setQuestionCount(questionCount+1)
+    fetch('https://api.themoviedb.org/3/person/' + actor.id + '/movie_credits?api_key=9e2b8a1d23b0a9148f8bb5bf8f512bd8&language=en-US', {
+        method: "GET",
+        dataType: "JSON",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        const isCorrect = data.cast.find(actorMovie => actorMovie.title === movie)
+    
+        if ((yes && isCorrect) || (!yes && !isCorrect)) {
+          setScore(score+1)
+        }
+    
+        setQuestionCount(questionCount+1)
+      })
   }
 
   return(
-    <div>
-      Did {questions[questionCount].actor} plays in {questions[questionCount].movie} ?
-      <button onClick={() => answer(true, props.actors[questionCount], questions[questionCount].movie)}>Yes</button>
-      <button onClick={() => answer(false, props.actors[questionCount], questions[questionCount].movie)}>No</button>
-      Score : {score}
+    <div className="questions__card">
+      <div className="questions__score">Score : {score}</div>
+      <div className="questions__question">Did <b>{questions[questionCount].actor}</b> play in <b>{questions[questionCount].movie}</b> ?</div>
+      <div className="questions__buttons-group">
+        <button className="questions__button questions__button--yes" onClick={() => answer(true, props.actors[questionCount], questions[questionCount].movie)}><img src="check.svg" /></button>
+        <button className="questions__button questions__button--no" onClick={() => answer(false, props.actors[questionCount], questions[questionCount].movie)}><img src="delete.svg" /></button>
+      </div>
     </div>
   )
 }
